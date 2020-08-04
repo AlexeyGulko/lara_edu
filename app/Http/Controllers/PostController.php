ad1b53e8a5fed954f130690cc0012e09b405b75f
Alexey Gulko <dev.gulkoalexey@gmail.com>
2020-08-04 14:19:00 +0300

    change models, require telescope, add phpdoc to postcontoller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,23 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePost;
 use App\Model\Post;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
 
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $posts = Post::latest()->get();
         return view('index', compact('posts'));
     }
 
+    /**
+     * @return Application|Factory|View
+     */
     public function create()
     {
         return view('post.create');
     }
 
+    /**
+     * @param StorePost $request
+     * @return Application|RedirectResponse|Redirector
+     */
     public function store(StorePost $request)
     {
         Post::create($request->validated());
@@ -30,8 +45,8 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return Response
+     * @param Post $post
+     * @return Application|Factory|View
      */
     public function show(Post $post)
     {
@@ -52,7 +67,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
      * @return Response
      */
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,33 +3,11 @@
 namespace App\Model;
 
 use App\Scopes\PublishScope;
+use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Model\Post
- *
- * @property int $id
- * @property string $slug
- * @property string $title
- * @property string $description
- * @property string $body
- * @property int $published
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post wherePublished($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Post whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class Post extends Model
 {
+    protected $fillable = ['title', 'slug', 'description', 'published'];
 
     public function getRouteKeyName()
     {

18653f21635915c9bf21414b50fc729e3b454e9a
Alexey Gulko <dev.gulkoalexey@gmail.com>
2020-08-12 12:20:27 +0300

    create morph relations with tags

--- app/Post.php
+++ app/Post.php
@@ -28,7 +28,7 @@ class Post extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->morphToMany(Tag::class, 'taggable');
     }
 
     public function owner()
--- app/Tag.php
+++ app/Tag.php
@@ -10,7 +10,7 @@ class Tag extends Model
 
     public function posts()
     {
-        return $this->belongsToMany(Post::class);
+        return $this->morphedByMany(Post::class, 'taggable');
     }
 
     public function getRouteKeyName()

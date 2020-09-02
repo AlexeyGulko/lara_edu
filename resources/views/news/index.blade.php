5c422955859771aeefbb03e19b5659927428c4f9
Alexey Gulko <dev.gulkoalexey@gmail.com>
2020-09-02 21:17:43 +0300

    fix news index

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="col-md-8">
         @forelse($news as $item)
-            <x-news.preview :newsItem="$item"></x-news.preview>
+            <x-news.preview :news="$item"></x-news.preview>
         @empty
             <p>Нет постов</p>
         @endforelse
